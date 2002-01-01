7944743978eccae6a95bb994d5c8b1a8d046c1c3
GregsonMedel <GregsonJamesMedel@Gmail.com>
2002-01-01 12:33:54 +0800

    fix some css issues

--- application/views/Diary/addNew.php
+++ application/views/Diary/addNew.php
@@ -2,19 +2,18 @@
 <div class="forms">
     <form action="<?= base_url('Diary/AddNewAction') ?>" method="post">
         <label for="title" >Title</label>
-        <input type="text" name="title">
+        <input type="text" name="title" value="<?= set_value('title'); ?>">
         <?= '<span class="error">' . form_error('title') . '</span>' ?>
 
         <label for="body">Body</label>
-        <textarea name="body" cols="50" rows="10"></textarea>
+        <textarea name="body" cols="50" rows="10" value="<?= set_value('body'); ?>"></textarea>
         <?= '<span class="error">' . form_error('body') . '</span>' ?>
 
         <label for="datepost">Date</label>
-        <input type="date" name="datepost">
+        <input type="date" name="datepost" value="<?= set_value('datepost'); ?>">
         <?= '<span class="error">' . form_error('datepost') . '</span>' ?>
 
         <input class="button" type="Submit" value="Submit">
-        <input class="button" type="Submit" value="Cancel">
         
     </form>
 </div>
